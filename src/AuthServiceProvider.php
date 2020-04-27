89828eb2d78eb0b14a816d8fffb29118f830f0f4
ramesh <ramesh@dzangolab.com>
2020-04-27 18:10:50 +0545

    chore: revert config name

--- src/AuthServiceProvider.php
+++ src/AuthServiceProvider.php
@@ -78,7 +78,7 @@ class AuthServiceProvider extends ServiceProvider
             ], 'auth-factories');
 
             $this->publishes([
-                __DIR__.'/../config/auth.php' => config_path('auth.php'),
+                __DIR__.'/../config/dzangolabAuth.php' => config_path('dzangolabAuth.php'),
             ], 'auth-config');
 
             $this->commands([
