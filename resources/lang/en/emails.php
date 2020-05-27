1a629d53f187ff4e916f62fbe8a6c4e95d6bff2c
ramesh <ramesh@dzangolab.com>
2020-05-27 18:32:03 +0545

    chore: add resources

---
+++ resources/lang/en/emails.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'user' => [
+        'subject' => [
+            'confirmation' => 'user confirmation',
+            'password_changed' => 'Password was changed',
+            'password_reset' => 'Reset your password',
+        ],
+    ],
+];
