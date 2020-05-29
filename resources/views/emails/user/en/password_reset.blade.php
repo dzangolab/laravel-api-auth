c8e524ff96dc7fd990031d2699b205c2de7b8f87
ramesh <ramesh@dzangolab.com>
2020-05-29 16:39:06 +0545

    fix: fix email templates

--- resources/views/emails/user/en/password_reset.blade.php
+++ resources/views/emails/user/en/password_reset.blade.php
@@ -2,7 +2,7 @@
 <body>
     <h1>Hi {{ $user->username }},</h1>
 
-    <p>Please <a href="{{ $url }}"><u>click here</u></a> to reset your demos account password.</p>
+    <p>Please <a href="{{ $url }}"><u>click here</u></a> to reset your account password.</p>
 
     <p>If you are not able to click on the link, please copy and paste:</p>
 
